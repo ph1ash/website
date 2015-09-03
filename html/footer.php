85e6f72df0c76f0fa89ea3b379f5310989837374
Matt Dresser <m.dres344@gmail.com>
2015-09-02 22:10:07 -0500

    Building out blurb index page.

---
+++ html/footer.php
@@ -0,0 +1,10 @@
+<footer id="footer">
+	<ul class="icons">
+		<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+		<li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
+		<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+	</ul>
+	<ul class="copyright">
+		<li>&copy; 2015 mattdresser.com</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+	</ul>
+</footer>
---
+++ html/thoughts.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+	<!--Head-->
+	<?php include 'head.php' ?>
+	<body id="top">
+		<!--Header-->
+		<?php include 'header.php' ?>
+		<div id="main">
+			<section id="one">
+			<?php 
+				$directory = "assets/blurbs";
+				$contents = scandir($directory);
+				if ($contents)
+				{
+					foreach($contents as $key => $value)
+					{
+						if ($value == "." || $value == "..")
+						{
+							unset($key);
+						}
+					}
+				}
+				echo "<ul>";
+				foreach ($contents as $k => $v)
+				{
+					echo "<li><a href=\"$directory/" . $v . "\">link text</a></li>";
+				}
+				echo "</ul>";
+			?>
+			</section>
+		</div>
+		<!-- Footer -->
+		<?php include 'footer.php' ?>
+		<!-- Scripts -->
+		<?php include 'scripts.php' ?>
+	</body>
+</html>
