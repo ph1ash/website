ed3aee306bf2b8d6766c5881c3bfe80a6540ef34
Matt Dresser <m.dres344@gmail.com>
2015-09-07 12:14:10 -0500

    Fixed thoughts.php so that it parses the directory properly and generates links with a better style

--- html/assets/blurbs/header_blurbs.php
+++
@@ -1,6 +0,0 @@
-<header id="header">
-	<a href="#" class="image avatar"><img src="../../images/avatar.jpg" alt="" /></a>
-	<h1><strong>Hi! I'm Matt</strong>, a creative engineer<br />
-	who enjoys playing with and making<br />
-	all sorts of electronics.</h1>
-</header>
--- html/thoughts.php
+++ html/thoughts.php
@@ -1,42 +1,44 @@
 <!DOCTYPE HTML>
 <html>
-	<!--Head-->
-	<?php include 'head.php' ?>
-	<body id="top">
-		<!--Header-->
-		<?php include 'header.php' ?>
-		<div id="main">
-			<section id="one">
-			<?php 
-				$directory = "assets/blurbs";
-				$contents = scandir($directory);
-				if ($contents)
-				{
-					foreach($contents as $key => $value)
-					{
-						if ($value == "." || $value == "..")
-						{
-							unset($key);
-						}
-					}
-				}
-				echo "<ul>";
-				foreach ($contents as $k => $v)
-				{
-					if($v != "." || $v != "..")
-					{						
-						$result = $v !== "." ;
-						echo "$result";
-						echo "<li><a href=\"$directory/" . $v . "\">link text</a></li>";
-					}
-				}
-				echo "</ul>";
-			?>
-			</section>
-		</div>
-		<!-- Footer -->
-		<?php include 'footer.php' ?>
-		<!-- Scripts -->
-		<?php include 'scripts.php' ?>
-	</body>
+    <!--Head-->
+    <?php include 'head.php' ?>
+    <body id="top">
+        <!--Header-->
+        <?php include 'header.php' ?>
+        <div id="main">
+            <section id="one">
+            <h1>Random Thoughts</h1>
+            <?php
+                $directory = "assets/blurbs";
+                $contents = scandir($directory);
+                if ($contents)
+                {
+                    foreach($contents as $key => $value)
+                    {
+                        if ($value == "." || $value == "..")
+                        {
+                            unset($key);
+                        }
+                    }
+                }
+                echo "<ul>";
+                foreach ($contents as $k => $v)
+                {
+                    if($v !== "." && $v !== "..")
+                    {
+                    	$pageName = str_replace('_',' ',$v);
+                        $pageName = str_replace(".php","",$pageName);
+                        $pageName = ucfirst($pageName);
+                        echo "<li><a href=\"$directory/" . $v . "\">$pageName</a></li>";
+                    }
+                }
+                echo "</ul>";
+            ?>
+            </section>
+        </div>
+        <!-- Footer -->
+        <?php include 'footer.php' ?>
+        <!-- Scripts -->
+        <?php include 'scripts.php' ?>
+    </body>
 </html>
